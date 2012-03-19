d9350d503907855468eacf0344be19c668e364e7
Emmanuel De Saint Steban <poubellemanu@gmail.com>
2012-03-19 14:59:45 +0100

    create twitter get provider

--- test/lib/TwitterTrendProvider.js
+++ test/lib/TwitterTrendProvider.js
@@ -21,11 +21,13 @@ function TwitterTrendProvider() {
      */
     this.getTwitterTrends = function (maxNbrTrend) {
         var returnListTrend = Array(maxNbrTrend);
+        console.log(this.listTrendTwitter);
         for (var currentTrend = 0; currentTrend < this.listTrendTwitter.length; currentTrend++) {
-            if (currentTrend = maxNbrTrend) {
+            if (currentTrend == maxNbrTrend) {
                break;
             }
             returnListTrend[currentTrend] = this.listTrendTwitter[currentTrend];
+            console.log(this.listTrendTwitter[currentTrend]);
         }
         return returnListTrend;
     }
@@ -36,7 +38,8 @@ function TwitterTrendProvider() {
             return this;
         }*/
         //Create a client (need to pass it these values??)
-        var client = http.createClient(8090, 'httpproxy.tf1.fr');
+        var client = http.createClient('80', 'edesaintsteban.devpalm.tf1.fr');
+       // var client = http.createClient(8090, 'httpproxy.tf1.fr');
 
 //Request with headers for basic auth, which Twitter also accepts in addition to oAuth
 //Note that we're using method 'statuses/filter' and parameter 'track'
@@ -44,19 +47,40 @@ function TwitterTrendProvider() {
 
         // /1/trends/23424819.json
 
-        var request = client.request('GET', '/', {
-            "host":"google.com"
-           /*, "Authorization":"Basic " + new Buffer('manudss' + ":" + 'calinette').toString('base64'), //Need to create buffer object and base64 encode it
-            "User-Agent": "Twitter-Node"*/
+        var request = client.request('GET', '/src/23424819.json', {
+            "host":"edesaintsteban.devpalm.tf1.fr"
+            //"host":"api.twitter.com"
         });
+
+        var self = this;
 
 
         request.on('response', function(response) {
             response.on('data', function(chunk) {
-                console.log('Twitter data ok')
-                console.log(chunk.toString()); //should be object with tweet information
+                console.log('Twitter data ok');
+                var jsonString = chunk.toString();
+                console.log(jsonString); //should be object with tweet information
+
+
+             var  TwitterJson = JSON.parse(jsonString);
+               console.log(TwitterJson[0].trends);
+                  if (TwitterJson[0].trends === undefined) {
+                 var error = new Error('trends is n ot defined in twitter trends');
+                 callBackFunction(error, null);
+                 }
+
+                var saveListTrend = [];
+
+                // Traitement de la réponse twitter, si le nombre max de request par hour n'est pas atteinds
+                for (var trend = 0; trend < TwitterJson[0].trends.length; trend++) {
+                    saveListTrend[trend] = TwitterJson[0].trends[trend].name;
+                }
+
+                self.listTrendTwitter = saveListTrend;
+
+
                 // Appel de la fonction de collback
-                callBackFunction(null, chunk.toString());
+                callBackFunction(null, self);
             });
             response.on('error', function(error) {
                 console.log('error');
@@ -72,11 +96,6 @@ function TwitterTrendProvider() {
         });
         request.end();
 
-
-        console.log(request);
-
-        // Appel de la fonction de collback
-        callBackFunction(null, this);
     }
 
 }
