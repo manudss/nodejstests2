852164b11cf8d17e97cfcd8b10c0b42798b45d9a
Emmanuel De Saint Steban <poubellemanu@gmail.com>
2012-03-19 15:00:58 +0100

    add twitter provider for index page

--- test/routes/index.js
+++ test/routes/index.js
@@ -8,10 +8,19 @@ var TwitterTrendProvider = require('../lib/TwitterTrendProvider.js');
 exports.index = function(req, res){
     var twitter =  new TwitterTrendProvider();
 
+
     twitter.updateTwitterTrends(function(err, twitter) {
- /*       console.log(err);
-        console.log(twitter);*/
-        res.write(twitter, 'string');
+       console.log(err);
+        if (err != null) {
+            res.write(err.message);
+        } else  if (twitter != null) {
+            console.log(twitter);
+            var twitterTrends = twitter.getTwitterTrends(5);
+            console.log(twitterTrends);
+            res.render('tendances', { topTwitter:  twitterTrends });
+        }
+
     })
-    //res.render('index', { msg: 'Hello' });
+
+
 };
